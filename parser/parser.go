2aee6ad559ad3c9b200f9d48c93e8516e16e48bb
zhaozhida <zhaozhida@qiniu.com>
2023-07-04 14:58:57 +0800

    feat: fix parser file

--- parser/parser.go
+++ parser/parser.go
@@ -2,7 +2,7 @@
  * @Author: easonchiu
  * @Date: 2023-07-03 11:07:23
  * @LastEditors: easonchiu
- * @LastEditTime: 2023-07-03 19:08:24
+ * @LastEditTime: 2023-07-04 14:36:16
  * @Description:
  */
 package parser
@@ -42,13 +42,25 @@ type APPData struct {
 	IOSIAPList          []*IOSIAP // ios 内购列表
 	// 华为市场
 	HWID               string // hw id
+	HWPackageID        string // hw package id
+	HWSupplier         string // hw 供应商名称
 	HWRate             string // hw 评分
 	HWRateCount        string // hw 评价数
 	HWLastVersion      string // hw 最新版本
 	HWLastUpdate       string // hw 最新版本时间
 	HWPackageSize      string // hw 包大小
 	HWPrivacyPolicyUrl string // hw 隐私政策地址
+	HWTargetSDK        string // hw 不知道是啥，感觉像第三方sdk的数量
 	HWOtherApps        []*App // hw 全部同主体的app
+	// 小米市场
+	MIExist       bool   // mi 是否有
+	MIRateCount   string // mi 评价数
+	MILastVersion string // mi 最新版本
+	MILastUpdate  string // mi 最新版本时间
+	// 应用宝
+	QQExist       bool   // QQ 是否有
+	QQLastVersion string // QQ 最新版本
+	QQLastUpdate  string // QQ 最新版本时间
 }
 
 func ParseAPPData(iosId string) (*APPData, error) {
@@ -89,7 +101,41 @@ func ParseAPPData(iosId string) (*APPData, error) {
 	appData.HWID = getHWAppId(appData.IOSName)
 	if appData.HWID != "" {
 		json := getHWAppData(appData.HWID)
+		appData.HWPackageID = getHWPackageID(json)
+		appData.HWSupplier = getHWSupplier(json)
 		appData.HWRate = getHWRate(json)
+		appData.HWRateCount = getHWRateCount(json)
+		appData.HWLastVersion = getHWLastVersion(json)
+		appData.HWLastUpdate = getHWLastUpdate(json)
+		appData.HWPackageSize = getHWPackageSize(json)
+		appData.HWTargetSDK = getHWTargetSDK(json)
+		appData.HWPrivacyPolicyUrl = getHWPrivacyPolicyUrl(json)
+		appData.HWOtherApps = getHWOtherApps(json, appData.HWID)
+	}
+
+	// 小米数据
+	if appData.HWPackageID != "" {
+		doc, err := getMIDoc(appData.HWPackageID)
+		if err == nil {
+			appData.MIExist = getMIExist(doc)
+			if appData.MIExist {
+				appData.MIRateCount = getMIRateCount(doc)
+				appData.MILastVersion = getMILastVersion(doc)
+				appData.MILastUpdate = getMILastUpdate(doc)
+			}
+		}
+	}
+
+	// 应用宝数据
+	if appData.HWPackageID != "" {
+		doc, err := getQQDoc(appData.HWPackageID)
+		if err == nil {
+			appData.QQExist = getQQExist(doc)
+			if appData.QQExist {
+				appData.QQLastVersion = getQQLastVersion(doc)
+				appData.QQLastUpdate = getQQLastUpdate(doc)
+			}
+		}
 	}
 
 	return appData, nil
